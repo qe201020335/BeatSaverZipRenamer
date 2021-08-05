e2f5cebf29e2046dbc0b2bd64985f15d1513735f
Sky Z <qe201020335@sina.com>
2021-08-05 01:47:06 -0400

    name fetch and format

---
+++ BeatSaverZipRenamer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using BeatSaverSharp;
+using BeatSaverSharp.Models;
+
+namespace BeatSaverZipRenamer
+{
+    internal class Program
+    {
+        private static BeatSaver bs;
+        private static string prgname;
+        private static async Task GetNewFileName(string hash)
+        {
+            Beatmap beatmap = await bs.BeatmapByHash(hash);
+            if (beatmap == null)
+            {
+                Console.WriteLine($"{hash} NOTFOUND or TIMEOUT.");
+                return;
+            }
+            string result = $"{beatmap.ID} ({beatmap.Metadata.SongName} - {beatmap.Metadata.LevelAuthorName}).zip";
+            Console.WriteLine(result);
+            // return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            AssemblyName ver = typeof(Program).Assembly.GetName();
+            prgname = ver.Name;
+            
+            int len = args.Length;
+            if (len == 0)
+            {
+                MessageBox.Show("Drag hash named zips on this tool.", prgname);
+                return;
+            }
+            
+            bs = new BeatSaver(ver.Name, ver.Version);
+
+            List<Task> tasks = new List<Task>();
+            
+            foreach (string arg in args)
+            {
+                string filename = Path.GetFileNameWithoutExtension(arg);
+                tasks.Add(GetNewFileName(filename));
+                Thread.Sleep(250);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            
+            bs.Dispose();
+            
+            MessageBox.Show("Done.", prgname);
+        }
+    }
+}
